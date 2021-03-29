61d1918370f3cfe6d536c254fb70912c923f5db3
Panda Guy <59342211+oscarpete@users.noreply.github.com>
2021-03-29 15:58:12 +0200

    complete with date as extra

---
+++ interactions/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace interactions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime todaysDate = DateTime.Now;
+            string userFeeling = null;
+            Console.WriteLine("Enter Username:");
+            string userName = Console.ReadLine(); //is this a comment?
+            Console.WriteLine($"Hello {userName} How are you? good or bad?");
+            userFeeling = Console.ReadLine();
+            
+            if(userFeeling == "good")
+            {
+                Console.WriteLine($"Hey {userName} today {todaysDate} I also feel {userFeeling}");
+            }
+            else
+            {
+                Console.WriteLine($"Well {userName} I know you are {userFeeling} and today {todaysDate} is a new day!");
+            }
+        }
+    }
+}
